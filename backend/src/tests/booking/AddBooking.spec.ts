797fa1319a6189f8e237f421f0823d72b74ce4f8
Ayrton <ayrtonsolis153@hotmail.com>
2025-08-15 10:45:43 -0300

    added .only

--- backend/src/tests/booking/AddBooking.spec.ts
+++ backend/src/tests/booking/AddBooking.spec.ts
@@ -2,7 +2,7 @@ import { BookingService } from "../../models/services/BookingService.js";
 import { BookingModel } from "../../models/request/BookingModel";
 import "chai/register-should.js";
 
-describe("Add Booking", () => {
+describe.only("Add Booking", () => {
   const bookingService = new BookingService();
 
   it("@Smoke - Add Booking Successfully", async () => {
