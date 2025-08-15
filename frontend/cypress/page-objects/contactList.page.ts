323e9d470d28f704717700d47ab77ac345b9acba
Juan Fagundez <juanfagundez96@gmail.com>
2025-08-15 12:46:31 -0300

    Revert "Jf/second training"

--- frontend/cypress/page-objects/contactList.page.ts
+++
@@ -1,7 +0,0 @@
-class ContactListPage {
-  get contactRow() {
-    return cy.get('tr.contactTableBodyRow');
-  }
-}
-
-export default new ContactListPage();
--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,19 +1,37 @@
-// Make sure this file is a module
-export {};
-
-Cypress.Commands.add('createUserViaApi', () => {
-  const randomStr = Math.random().toString(36).substring(2, 10);
-  const body = {
-    email: `user_${randomStr}@test.com`,
-    firstName: 'Test',
-    lastName: 'Test',
-    password: '1234567',
-  };
-
-  return cy.request({
-    method: 'POST',
-    url: 'https://thinking-tester-contact-list.herokuapp.com/users',
-    body,
-    failOnStatusCode: false,
-  });
-});
+/// <reference types="cypress" />
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       login(email: string, password: string): Chainable<void>
+//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+//     }
+//   }
+// }
